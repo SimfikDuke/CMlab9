3af22e764da085248484f8d5f5fd40ffefe5e838
SimfikDuke <simfikduke@gmail.com>
2018-03-28 16:18:23 +0300

    With comments

--- CMlab9.py
+++ CMlab9.py
@@ -2,9 +2,10 @@ from numpy import linalg as la
 import numpy as np
 from det import det
 
-a = [[2,3],
-     [1,4]]
-e = 1e-2
+a = [[2, 3],
+     [1, 4]]  # Start matrix
 
-d = det(a)
+e = 1e-2  # Epsilon
+
+d = det(a, False)  # Max = False, Min = True
 d.epscals(e)
